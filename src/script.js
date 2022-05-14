72bd0eba8fef996e5b63c09577d033cb889819a3
cheezq <cheezq4@gmail.com>
2022-05-15 04:35:15 +0800

    Initial Draft

---
+++ src/script.js
@@ -0,0 +1,26 @@
+let intro = document.querySelector('.intro')
+let intro_welcome = document.querySelector('.intro-welcome')
+let welcome = document.querySelectorAll('.welcome')
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        welcome.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 300)
+        });
+
+        setTimeout(() => {
+            welcome.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 100)
+            })
+        }, 2000)
+
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 2200)
+    })
+})
